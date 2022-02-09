769804c6ee80ad4cb357fcd1d3563be63a310f30
Henry-Johnson-100 <hencutJohnson@gmail.com>
2022-02-08 20:34:30 -0600

    Substantially altered the syntax data types to carry more information in the syntax trees after parsing to aid in static analysis

--- app/Parser/Syntax.hs
+++ app/Parser/Syntax.hs
@@ -1,263 +1,211 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use newtype instead of data" #-}
+
 module Parser.Syntax
-  ( Keyword (..),
-    Data (..),
-    BracketTerminal (..),
-    ScopeType (..),
-    SyntaxTree (..),
-    Source (..),
-    SyntaxUnit (..),
-    Token (..),
-    TokenSource (..),
-    isPrimitive,
-    isString,
-    isNumeric,
-    isId,
-    unNum,
-    unString,
-    unBoolean,
-    unId,
-    keywords,
-    keywordRequiresId,
-    fromBracket,
-    fromScopeType,
-    fromTerminal,
-    baseData,
-    baseKeyword,
-    getTokenBracketScopeType,
-    genericKeyword,
-    genericBracket,
-    genericData,
-    dataTokenIsId,
-    keywordTokenIsDeclarationRequiringId,
-    sourceToSyntaxUnit,
+  ( StreamUnit (..),
+    DocStream (..),
+    CharUnit (..),
+    TokenStreamUnit (..),
+    SknId (..),
+    SknBracketTerminal (..),
+    SknScopeType (..),
+    SknBracketType (..),
+    SknBracket (..),
+    SknKeyword (..),
+    SknFlag (..),
+    SknType (..),
+    SknValLiteral (..),
+    SknData (..),
+    SknStaticTreeLabel (..),
+    SknTree (..),
+    SknSyntaxUnit (..),
+    sknData,
+    reservedWords,
+    tsutosu,
+    isKeywordRequiringId,
   )
 where
 
-import qualified Control.Monad as CMonad
+import qualified Data.Char
 import qualified Data.List
-import qualified Data.Maybe as DMaybe
 import qualified Util.Classes as UC
 import Util.General ((.<))
-import qualified Util.General as UGen
 import qualified Util.Tree as Tree
 
 ----Data definitions----------------------------------------------------------------------
 ------------------------------------------------------------------------------------------
 
-data Source a = Source {sourceUnit :: a, sourceLine :: Int} deriving (Show, Eq, Read)
+-- | Stream of some data, probably Char, not really used for anything
+type DocStream = String
 
-data SyntaxUnit = SyntaxUnit
-  { token :: !Token,
-    line :: !Int,
-    context :: !ScopeType
+-- | Holds information about a single unit from a RawStream
+data StreamUnit a = StreamUnit
+  { streamUnit :: a,
+    streamUnitLine :: Int,
+    streamUnitColumn :: Int
   }
   deriving (Show, Eq)
 
-data Token
-  = Bracket !ScopeType !BracketTerminal
-  | Data !Data
-  | Keyword !Keyword
-  deriving (Show, Read, Eq)
+type CharUnit = StreamUnit Char
 
-data BracketTerminal = Open | Close deriving (Show, Read, Eq)
+type TokenStreamUnit = StreamUnit SknToken
 
-data ScopeType = Send | Return deriving (Show, Read, Eq)
+data SknId = SknId !String deriving (Show, Eq, UC.Format)
 
-data Keyword
+data SknBracketTerminal = Open | Close deriving (Show, Read, Eq, UC.Format)
+
+data SknScopeType = Send | Return deriving (Show, Read, Eq, UC.Format)
+
+data SknBracketType = Value | Type deriving (Show, Read, Eq, UC.Format)
+
+data SknBracket = SknBracket
+  { sknBracketType :: !SknBracketType,
+    sknBracketScope :: !SknScopeType,
+    sknBracketTerminal :: !SknBracketTerminal
+  }
+  deriving (Show, Eq)
+
+data SknKeyword
   = Fish
   | School
   | Shoal
   | Swim
+  | Piscis
   | Lamprey
-  deriving (Show, Read, Eq)
+  | Fin
+  | Mackerel
+  deriving (Show, Read, Eq, UC.Format, Enum)
 
-data Data
-  = Num !Double
-  | String !String
-  | Boolean !Bool
-  | Id !String
-  | Null
-  deriving (Show, Read, Eq)
+data SknFlag
+  = Impure
+  | TailCall
+  | IR
+  | TypeUnsafe
+  deriving (Show, Read, Eq, UC.Format, Enum)
 
-----Type synonyms-------------------------------------------------------------------------
-------------------------------------------------------------------------------------------
+-- | Describing types, which can be possessed by all tokens and also by literals
+data SknType
+  = SknTInteger
+  | SknTDouble
+  | SknTChar
+  | SknTString
+  | SknTBool
+  | SknTVar !String
+  | SknTStruct !String
+  | SknTConstraint !String !SknType
+  | SknTUntyped
+  deriving (Show, Eq, UC.Format)
 
-type TokenSource = Source Token
+-- | Describing values' literal construction
+data SknValLiteral
+  = SknVInteger !Integer
+  | SknVDouble !Double
+  | SknVChar !Char
+  | SknVString !String
+  | SknVBool !Bool
+  | SknVList ![SknValLiteral]
+  deriving (Show, Eq, UC.Format)
 
-type SyntaxTree = Tree.Tree SyntaxUnit
+data SknData = SknData {sknDataLiteral :: !SknValLiteral, sknDataType :: !SknType}
+  deriving (Show, Eq, UC.Format)
+
+data SknStaticTreeLabel
+  = Literal
+  | FishDef
+  | SchoolDef
+  | ShoalDef
+  | SwimExpr
+  | FinExpr
+  | MackerelExpr
+  | LampreyExpr
+  | IdCallExpr
+  | PiscisDef
+  deriving (Show, Eq, UC.Format)
+
+data SknTree = SknTree !SknStaticTreeLabel !SyntaxTree deriving (Show, Eq)
+
+type SyntaxTree = Tree.Tree SknSyntaxUnit
+
+data SknToken
+  = SknTokenBracket !SknBracket
+  | SknTokenData !SknData
+  | SknTokenKeyword !SknKeyword
+  | SknTokenFlag !SknFlag
+  | SknTokenId !SknId
+  deriving (Show, Eq, UC.Format)
+
+data SknSyntaxUnit = SknSyntaxUnit
+  { sknUnitToken :: !SknToken,
+    sknUnitLine :: !Int,
+    sknUnitContext :: !SknScopeType
+  }
+  deriving (Show, Eq)
+
+-- | simpler constructor for SknData from just a provided SknValLiteral
+sknData :: SknValLiteral -> SknData
+sknData svl =
+  SknData
+    svl
+    ( case svl of
+        SknVInteger _ -> SknTInteger
+        SknVDouble _ -> SknTDouble
+        SknVChar _ -> SknTChar
+        SknVString _ -> SknTString
+        SknVBool _ -> SknTBool
+        SknVList _ -> SknTStruct "List"
+    )
 
 ----Instances-----------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------
+instance (UC.Format a) => UC.Format (StreamUnit a) where
+  format (StreamUnit x line col) =
+    UC.format x
+      ++ concat [" on line: ", show line, " and column: ", show col]
 
-----Like
+instance UC.Format SknBracket where
+  format (SknBracket brType scope term) =
+    appendInOrder term (getScopeAngleBracket scope) (getTerminalBracket brType term)
+    where
+      getScopeAngleBracket Send = ">"
+      getScopeAngleBracket _ = "<"
+      getTerminalBracket Type _ = ":"
+      getTerminalBracket _ Open = "("
+      getTerminalBracket _ Close = ")"
+      appendInOrder Open abr terml = abr ++ terml
+      appendInOrder _ abr terml = terml ++ abr
 
-instance UC.Like Data where
-  (Num _) `like` (Num _) = True
-  (String _) `like` (String _) = True
-  (Boolean _) `like` (Boolean _) = True
-  (Id _) `like` (Id _) = True
-  Null `like` Null = True
-  _ `like` _ = False
+instance UC.Format SknTree where
+  format (SknTree label tr) = concat [UC.format label, ":\n", UC.format tr]
 
-instance UC.Like Keyword where
-  like = (==)
-
-instance UC.Like Token where
-  like (Bracket _ _) (Bracket _ _) = True
-  like (Data _) (Data _) = True
-  like (Keyword _) (Keyword _) = True
-  like _ _ = False
+instance UC.Format SknSyntaxUnit where
+  format (SknSyntaxUnit token line context) =
+    Data.List.intercalate " | " [UC.format token, show line, UC.format context]
 
 ----Defaultable
 
-instance UC.Defaultable Data where
-  defaultValue = Null
+-- | Produces an untyped False value.
+instance UC.Defaultable SknData where
+  defaultValue = SknData (SknVBool False) SknTUntyped
 
-instance UC.Defaultable SyntaxUnit where
-  defaultValue = SyntaxUnit (Data Null) 0 Return
-
-----Format
-
-instance UC.Format ScopeType where
-  format = show
-
-instance UC.Format BracketTerminal where
-  format = show
-
-instance UC.Format Data where
-  format (String a) = a
-  format (Num a) = show a
-  format (Boolean a) = show a
-  format (Id a) = a
-  format Null = ""
-
-instance UC.Format Keyword where
-  format Fish = "fish"
-  format School = "school"
-  format Shoal = "shoal"
-  format Swim = "swim"
-  format Lamprey = "lamprey"
-
-instance UC.Format Token where
-  format (Bracket st bt) = fromBracket st bt
-  format (Data d) = UC.format d
-  format (Keyword k) = UC.format k
-
-instance (UC.Format a) => UC.Format (Source a) where
-  format (Source x l) = UC.format x ++ ": " ++ UC.format l
-
-instance UC.Format SyntaxUnit where
-  format (SyntaxUnit t l c) =
-    Data.List.intercalate " | " $ [UC.format t, UC.format l, UC.format c]
+instance UC.Defaultable SknSyntaxUnit where
+  defaultValue =
+    SknSyntaxUnit (SknTokenData UC.defaultValue) 0 Return
 
 ----Data functions------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------
 
-primTypeOf :: Data -> String
-primTypeOf (String _) = "String"
-primTypeOf (Num _) = "Num"
-primTypeOf (Boolean _) = "Boolean"
-primTypeOf (Id _) = "Id"
-primTypeOf Null = "Null"
+reservedWords :: [String]
+reservedWords = reservedSknKeywords ++ reservedSknFlags
+  where
+    reservedSknKeywords = (map (toLower . show) . enumFrom) Fish
+    reservedSknFlags = (map (toLower . show) . enumFrom) Impure
+    toLower = map Data.Char.toLower
 
-isId :: Data -> Bool
-isId (Id _) = True
-isId _ = False
+-- | TokenStremaUnit to SknSyntaxUnit
+tsutosu :: SknScopeType -> TokenStreamUnit -> SknSyntaxUnit
+tsutosu st (StreamUnit t l _) = SknSyntaxUnit t l st
 
-isPrimitive :: Data -> Bool
-isPrimitive d = any (d `UC.like`) [Num 0.0, String "", Boolean True, Null]
-
-isNumeric :: Data -> Bool
-isNumeric = UC.like (Num 0.0)
-
-isString :: Data -> Bool
-isString (String _) = True
-isString _ = False
-
-unNum :: Data -> Maybe Double
-unNum (Num x) = Just x
-unNum Null = Just 0.0
-unNum _ = Nothing
-
-unString :: Data -> Maybe String
-unString (String s) = Just s
-unString Null = Just ""
-unString _ = Nothing
-
-unBoolean :: Data -> Maybe Bool
-unBoolean (Boolean b) = Just b
-unBoolean Null = Just False
-unBoolean _ = Nothing
-
-unId :: Data -> Maybe String
-unId (Id d) = Just d
-unId _ = Nothing
-
-----Keyword functions---------------------------------------------------------------------
-------------------------------------------------------------------------------------------
-
-keywords :: [String]
-keywords = map UC.format [Fish, School, Shoal, Swim, Lamprey]
-
-keywordRequiresId :: Keyword -> Bool
-keywordRequiresId k = if elem k [Swim, Lamprey] then False else True
-
-----Bracket functions---------------------------------------------------------------------
-------------------------------------------------------------------------------------------
-
-fromScopeType :: ScopeType -> Char
-fromScopeType Send = '>'
-fromScopeType Return = '<'
-
-fromTerminal :: BracketTerminal -> Char
-fromTerminal Open = '('
-fromTerminal Close = ')'
-
-fromBracket :: ScopeType -> BracketTerminal -> String
-fromBracket =
-  CMonad.liftM2
-    (:)
-    (fromScopeType . fst)
-    ( UGen.listSingleton
-        . fromTerminal
-        . snd
-    )
-    .< (,)
-
-----Token functions-----------------------------------------------------------------------
-------------------------------------------------------------------------------------------
-
-baseData :: Token -> DMaybe.Maybe Data
-baseData (Data d) = DMaybe.Just d
-baseData _ = DMaybe.Nothing
-
-baseKeyword :: Token -> DMaybe.Maybe Keyword
-baseKeyword (Keyword k) = DMaybe.Just k
-baseKeyword _ = DMaybe.Nothing
-
-getTokenBracketScopeType :: Token -> DMaybe.Maybe ScopeType
-getTokenBracketScopeType (Bracket st _) = DMaybe.Just st
-getTokenBracketScopeType _ = DMaybe.Nothing
-
-genericKeyword :: Token
-genericKeyword = Keyword Fish
-
-genericBracket :: Token
-genericBracket = Bracket Send Open
-
-genericData :: Token
-genericData = Data (Num 0)
-
-dataTokenIsId :: Token -> Bool
-dataTokenIsId (Data (Id _)) = True
-dataTokenIsId _ = False
-
-keywordTokenIsDeclarationRequiringId :: Token -> Bool
-keywordTokenIsDeclarationRequiringId t =
-  DMaybe.maybe False (keywordRequiresId) (baseKeyword t)
-
-----Source functions----------------------------------------------------------------------
-------------------------------------------------------------------------------------------
-
-sourceToSyntaxUnit :: TokenSource -> ScopeType -> SyntaxUnit
-sourceToSyntaxUnit (Source t ln) = SyntaxUnit t ln
+isKeywordRequiringId :: SknKeyword -> Bool
+isKeywordRequiringId k = or $ (<$>) (k ==) [Fish, School, Shoal]
